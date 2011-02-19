c69c4a672ca39a8345729a8168483a04b4f26d56
bow <bow@bow.web.id>
2011-02-19 20:20:27 +0100

    added method to extract tags

--- abifpy/__init__.py
+++ abifpy/__init__.py
@@ -2,8 +2,17 @@
 
 import struct
 
-fmt_head = '>4sH4sIHHIII'
-fmt_dir = '>4sIHHIIII'
+# directory data structure:
+# tag name, tag number, element type code, element size, number of elements
+# data size, data offset, handle
+FMT_DIR = '>4sIHHIIII'
+# for header +file type, file version
+FMT_HEAD = '>4sH4sIHHIII'
+# only retrieve the data we care about:
+# plate barcode, color data, machine model, sample tracking id, sequence, 
+# peak locations, max quality value, trim probability
+TAGS = ('CTID', 'DATA', 'HCFG', 'LIMS', 'PBAS', 'PLOC', 'phQL', 'phTR')
+
 
 class Trace(object):
 
@@ -14,16 +23,16 @@ class Trace(object):
             self._data = source.read()
 
         if self._data[:4] == 'ABIF':
-            self._header = struct.unpack(fmt_head, self._data[:30])
+            self._header = struct.unpack(FMT_HEAD, self._data[:30])
             self.version = self._header[1]
             self.elemsize = self._header[5]
             self.elemnum = self._header[6]
             self.offset = self._header[8]
             self.tags = {}
 
-            for i in self.gen_dir():
-                if (i[:4], 
-
+            for entry in self.gen_dir():
+                if entry[0] in TAGS:
+                    self.tags[entry[0] + str(entry[1]).zfill(3)] = entry
         else:
             del self._data
             print "File error. Make sure the file is a proper .ab1 file."
@@ -33,7 +42,7 @@ class Trace(object):
         while index < self.elemnum:
             start = self.offset + index * self.elemsize
             finish = self.offset + (index + 1) * self.elemsize
-            yield struct.unpack(fmt_dir, self._data[start:finish])
+            yield struct.unpack(FMT_DIR, self._data[start:finish])
             index += 1
 
 
