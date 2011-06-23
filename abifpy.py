f81d15756f5f212ecdbdfdbbf9beb550edcf2cc8
bow <bow@bow.web.id>
2011-06-23 03:10:08 +0200

    added unittests, new examples, further tweaks on tag data parsing

--- abifpy.py
+++ abifpy.py
@@ -2,14 +2,22 @@
 
 """Python module for reading .ab1 trace files"""
 
+import datetime
 import re
 import struct
-import datetime
+import sys
 
 # dictionary for deciding which values to extract and contain in self.meta
 TAGS = {
+            'DATA1':'raw1',
+            'DATA2':'raw2',
+            'DATA3':'raw3',
+            'DATA4':'raw4',
+            'DySN1':'dyename',
+            'GTyp1':'polymer',
+            'FWO_1':'baseorder',
             'MODL1':'model', 
-            'SMPL1':'sampleid', 
+            'SMPL1':'samplename', 
             'TUBE1':'well',
        } 
 
@@ -17,41 +25,42 @@ __version__ = '0.3'
 
 class Trace(object):
     """Class representing trace file"""
-    def __init__(self, in_file, trimming=False):        
-        try:
-            with open(in_file) as source:
-                self._raw = source.read()
-            if not self._raw[:4] == 'ABIF':
-                raise IOError('Input is not a valid .ab1 trace file')
-        except IOError as (strerror):
-            print "IOError: {0}".format(strerror)
-        else:
-            # header data structure:
-            # file type, file, version, tag name, tag number, element type code,
-            # element size, number of elements, data size, data offset, handle,
-            # file type, file version
-            fmt_head = '>4sH4sIHHIII'
-            # dictionary for containing file metadata
-            self.meta = {}
-            # dictionary for containing extracted directory data
-            self.data = {}
-            self.meta['id'] = in_file.replace('.ab1','')
-            self.trimming = trimming
-            # values contained in file header
-            self._header = struct.unpack(fmt_head, self._raw[:30])
-            # file format version
-            self.version = self._header[1]
+    def __init__(self, inFile, trimming=False):        
+        with open(inFile) as source:
+            self._raw = source.read()
+            try:
+                if not self._raw[:4] == 'ABIF':
+                    raise IOError('Input is not a valid trace file')
+            except IOError:
+                source = None
+                raise
+            else:
+                # header data structure:
+                # file type, file, version, tag name, tag number, element type code,
+                # element size, number of elements, data size, data offset, handle,
+                # file type, file version
+                fmtHead = '>4sH4sIHHIII'
+                # dictionary for containing file metadata
+                self.meta = {}
+                # dictionary for containing extracted directory data
+                self.data = {}
+                self.meta['id'] = inFile.replace('.ab1','').replace('.fsa','')
+                self.trimming = trimming
+                # values contained in file header
+                self._header = struct.unpack(fmtHead, self._raw[:30])
+                # file format version
+                self.version = self._header[1]
 
-            # build dictionary of data tags
-            for entry in self._parse():
-                self.data[entry[0] + str(entry[1])] = entry
+                # build dictionary of data tags
+                for entry in self._parse():
+                    self.data[entry[0] + str(entry[1])] = entry
 
-            # retrieve attributes from tags
-            for item in self.data.keys():
-                # only extract data from tags we care about
-                if item in TAGS:
-                    # e.g. self.meta['well'] = 'B6'
-                    self.meta[TAGS[item]] = self.get_data(item)
+                # retrieve attributes from tags
+                for item in self.data.keys():
+                    # only extract data from tags we care about
+                    if item in TAGS:
+                        # e.g. self.meta['well'] = 'B6'
+                        self.meta[TAGS[item]] = self.getData(item)
     
     def _parse(self):
         """Generator for directory contents."""
@@ -59,74 +68,80 @@ class Trace(object):
         # file type, file, version, tag name, tag number, 
         # element type code, element size, number of elements
         # data size, data offset, handle
-        fmt_head = '>4sIHHIIII'
-        head_elemsize = self._header[5]
-        head_elemnum = self._header[6]
-        head_offset = self._header[8]
+        fmtHead = '>4sIHHIIII'
+        headElemSize = self._header[5]
+        headElemNum = self._header[6]
+        headOffset = self._header[8]
         index = 0
         
-        while index < head_elemnum:
-            start = head_offset + index * head_elemsize
-            finish = head_offset + (index + 1) * head_elemsize
+        while index < headElemNum:
+            start = headOffset + index * headElemSize
+            finish = headOffset + (index + 1) * headElemSize
             # added directory offset to tuple
             # to handle directories with data size <= 4 bytes
-            yield struct.unpack(fmt_head, self._raw[start:finish]) + (start,)
+            yield struct.unpack(fmtHead, self._raw[start:finish]) + (start,)
             index += 1
 
-    def get_data(self, dir_entry):
+    def getData(self, tagEntry):
         """Extracts data from directories in the file."""
-        tag_name, tag_no, elem_code, elem_size, elem_num, dir_size, dir_offset, dir_handle, data_offset = self.data[dir_entry]
+        tagName, tagNum, elemCode, elemSize, elemNum, dirSize, dirOffset, dirHandle, tagOffset = self.data[tagEntry]
 
         # if data size is <= 4 bytes, data is stored inside the directory
         # so offset needs to be changed
-        if dir_size <= 4:
-            dir_offset = data_offset + 20
+        if dirSize <= 4:
+            dirOffset = tagOffset + 20
 
-        if elem_code == 1:
-            fmt = ">{0}b".format(elem_num)
+        if elemCode == 1:
+            fmt = ">{0}b".format(elemNum)
             data = struct.unpack(fmt, 
-                    self._raw[dir_offset:dir_offset+dir_size])[0]
-        elif elem_code == 2:
-            fmt = ">{0}s".format(elem_num)
+                    self._raw[dirOffset:dirOffset+dirSize])[0]
+        elif elemCode == 2:
+            fmt = ">{0}s".format(elemNum)
             data = struct.unpack(fmt, 
-                    self._raw[dir_offset:dir_offset+dir_size])[0]
-        elif elem_code == 4:
-            fmt = ">{0}h".format(elem_num)
+                    self._raw[dirOffset:dirOffset+dirSize])[0]
+        elif elemCode == 4:
+            fmt = ">{0}h".format(elemNum)
             data = struct.unpack(fmt, 
-                    self._raw[dir_offset:dir_offset+dir_size])
-        elif elem_code == 5:
-            fmt = ">{0}l".format(elem_num)
+                    self._raw[dirOffset:dirOffset+dirSize])
+        elif elemCode == 5:
+            fmt = ">{0}l".format(elemNum)
             data = struct.unpack(fmt, 
-                    self._raw[dir_offset:dir_offset+dir_size])[0]
-        elif elem_code == 7:
-            fmt = ">{0}f".format(elem_num)
+                    self._raw[dirOffset:dirOffset+dirSize])[0]
+        elif elemCode == 7:
+            fmt = ">{0}f".format(elemNum)
             data = struct.unpack(fmt,
-                    self._raw[dir_offset:dir_offset+dir_size])[0]
-        elif elem_code == 10:
+                    self._raw[dirOffset:dirOffset+dirSize])[0]
+        elif elemCode == 10:
             fmt = ">hbb"
             year, month, date = struct.unpack(fmt,
-                    self._raw[dir_offset:dir_offset+dir_size])
+                    self._raw[dirOffset:dirOffset+dirSize])
             data = datetime.date(year, month, date)
-        elif elem_code == 11:
+        elif elemCode == 11:
             fmt = ">4b"
             hour, minute, second, hsecond = struct.unpack(fmt,
-                    self._raw[dir_offset:dir_offset+dir_size])
+                    self._raw[dirOffset:dirOffset+dirSize])
             data = datetime.time(hour, minute, second, hsecond)
-        elif elem_code == 18:
-            fmt = ">{0}s".format(elem_num)
+        elif elemCode == 12:
+            fmt = ">iibb"
             data = struct.unpack(fmt,
-                    self._raw[dir_offset:dir_offset+dir_size])[0].strip()
-        elif elem_code == 19:
-            fmt = str(dir_size-1) + 's'
+                    self._raw[dirOffset:dirOffset+dirSize])[0]
+        elif elemCode == 18:
+            fmt = ">{0}s".format(elemNum-1)
             data = struct.unpack(fmt,
-                    self._raw[dir_offset:dir_offset+dir_size-1])[0]
+                    self._raw[dirOffset+1:dirOffset+dirSize])[0].strip()
+        elif elemCode == 19:
+            fmt = ">{0}s".format(elemNum-1)
+            data = struct.unpack(fmt,
+                    self._raw[dirOffset:dirOffset+dirSize-1])[0]
+        elif elemCode == 1024:
+            data = 'not available'
         else:
             data = None
         return data
     
     def seq(self):
         """Returns sequence contained in the trace file."""
-        data = self.get_data('PBAS2')
+        data = self.getData('PBAS2')
         seq = re.sub("K|Y|W|M|R|S",'N',data)
 
         if self.trimming:
@@ -140,23 +155,23 @@ class Trace(object):
         Keyword argument:
         char -- True: returns ascii representation of phred values, False: returns phredvalues
         """
-        data = self.get_data('PCON2')
-        qual_list = []
+        data = self.getData('PCON2')
+        qualList = []
 
         if not char:    
             for value in data:
-                qual_list.append(str(ord(value)))
+                qualList.append(str(ord(value)))
         else:
             for value in data:
                 if ord(value) > 93:
                     value = chr(93)
-                qual_list.append(chr(ord(value) + 33))
+                qualList.append(chr(ord(value) + 33))
         # return value is list to make it compatible with the char option
         # and with the write() function (for writing .qual files)
         if self.trimming:
-            return self.trim(qual_list)
+            return self.trim(qualList)
         else:
-            return qual_list
+            return qualList
 
     def seqrecord(self):
         """Returns a SeqRecord object of the trace file."""
@@ -175,38 +190,38 @@ class Trace(object):
             print 'Biopython was not detected. No SeqRecord was created.'
             return None
 
-    def export(self, out_file="", qual=0):
+    def export(self, outFile="", qual=0):
         """Writes the trace file sequence to a fasta file.
         
         Keyword argument:
-        out_file -- output file name (detault 'tracefile'.fa)
+        outFile -- output file name (detault 'tracefile'.fa)
         qual -- 0: write fasta file, 1: write qual file, 2: write fastq file
 
         """
         
-        if out_file == "":
-            file_name = self.meta['id']
+        if outFile == "":
+            fileName = self.meta['id']
             if qual == 0:
-                file_name += '.fa'
+                fileName += '.fa'
             elif qual == 1:
-                file_name += '.qual'
+                fileName += '.qual'
             elif qual == 2:
-                file_name += '.fq'
+                fileName += '.fq'
         else:
-            file_name = out_file
+            fileName = outFile
         
         if qual == 0:
             contents = '>{0} {1}\n{2}\n'.format(
-                        file_name, self.meta['sampleid'], self.seq())
+                        fileName, self.meta['sampleid'], self.seq())
         elif qual == 1:
             contents = '>{0} {1}\n{2}\n'.format(
-                        file_name, self.meta['sampleid'], ' '.join(self.qual(char=False)))
+                        fileName, self.meta['sampleid'], ' '.join(self.qual(char=False)))
         elif qual == 2:
             contents = '@{0} {1}\n{2}\n+{0} {1}\n{3}\n'.format(
-                        file_name, self.meta['sampleid'], self.seq(), ''.join(self.qual()))
+                        fileName, self.meta['sampleid'], self.seq(), ''.join(self.qual()))
 
-        with open(file_name, 'w') as out_file:
-            out_file.writelines(contents)
+        with open(fileName, 'w') as outFile:
+            outFile.writelines(contents)
 
     def trim(self, seq, cutoff=0.05):
         """Trims the sequence using Richard Mott's modified trimming algorithm.
@@ -224,37 +239,37 @@ class Trace(object):
         take = False
         # set segment size for calculating segment score
         segment = 20
-        trim_start = 0
-        trim_finish = len(seq)
+        trimStart = 0
+        trimFinish = len(seq)
         
         if len(seq) <= segment:
-            print "Sequence length for {0} is shorter than trim segment size ({1}). Sequence not trimmed.".format(self.meta['id'], segment)
+            raise ValueError('Sequence can not be trimmed because it is shorter than the trim segment size')
         else:
             # calculate score for trimming
-            score_list =  []
+            scoreList =  []
             # actually the same as seq.qual(), but done to avoid infinite recursion
-            qual_hack = [ord(x) for x in self.get_data('PCON2')]
+            qualHack = (ord(x) for x in self.getData('PCON2'))
             
-            for qual in qual_hack:
+            for qual in qualHack:
                 # calculate probability back from formula used
                 # to calculate phred qual values
                 prob = 10 ** (qual/-10.0)
-                score_list.append(cutoff - prob)
+                scoreList.append(cutoff - prob)
             # algorithm for obtaining trimming position values
             for index in xrange(len(seq)-segment):
                 # calculate segment score
                 # if segment score is negative, trim the region out
-                segment_score = reduce(lambda x,y:x+y,
-                                 score_list[index:index+segment])
-                if not take and segment_score > 0:
-                    trim_start = index
+                segmentScore = reduce(lambda x,y:x+y,
+                                 scoreList[index:index+segment])
+                if not take and segmentScore > 0:
+                    trimStart = index
                     take = True
-                elif take and segment_score <= 0:
+                elif take and segmentScore <= 0:
                     # if segment length is longer than remaining bases
                     # take up everything until the end
                     if index + segment <= len(seq):
-                        trim_finish = index + segment - 1
+                        trimFinish = index + segment - 1
                         break
                     else:
                         break
-            return seq[trim_start:trim_finish]
+            return seq[trimStart:trimFinish]
